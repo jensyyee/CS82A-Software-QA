65e92994e2c99992d436796aee7f8b0f8573d357
Jennifer Yee <jensyyee@yahoo.com>
2015-06-15 23:12:36 -0700

    Use xpath for links with duplicates in main nav
    Links with duplicates in the main navigation bar need to be handled with xpath.

--- saucenav1.py
+++ saucenav1.py
@@ -5,16 +5,20 @@ import unittest, time, re
 
 links_landing_titles ={
     "RESOURCES" : "Resources",
-    "PRICING" : "Sauce Labs: Pricing",
-    "FEATURES" : "Features",
-    "DOCS" : "Sauce Labs Docs",
     "SIGN UP" : "Sauce Labs: Sign Up for a Free Trial",
     "COMPANY" : "Values",
-    "ENTERPRISE" : "Sauce Labs: Enterprise-grade testing on Sauce",
     "LOGIN" : "Login",
     "COMMUNITY" : "Open Sauce",
     "SOLUTIONS" : "Selenium Testing by Sauce Labs",
     }
+
+#Titles with dupicate in the main nav bar: they need to be handled with xpath.
+links_by_xpath = {
+    "(//a[contains(text(),'Enterprise')])[3]": "Sauce Labs: Enterprise-grade testing on Sauce",
+    "(//a[contains(text(),'Pricing')])[3]": "Sauce Labs: Pricing",
+    "(//a[contains(text(),'Features')])[3]": "Features",
+    "(//a[contains(text(),'Docs')])[3]": "Sauce Labs Docs",
+}
 
 class Saucenav1(unittest.TestCase):
     def setUp(self):
@@ -27,6 +31,16 @@ class Saucenav1(unittest.TestCase):
     def test_saucenav(self):
         driver = self.driver
 
+        for link in links_by_xpath:
+            driver.get(self.base_url)
+            driver.find_element_by_css_selector("a.hamburger").click()
+            driver.find_element_by_xpath(link).click()
+            expected_title = links_by_xpath[link]
+            try:
+                self.assertRegexpMatches(driver.title, expected_title)
+            except AssertionError as e:
+                self.verificationErrors.append(str(e))
+                
         for link in links_landing_titles: 
             driver.get(self.base_url)
             driver.find_element_by_xpath("//li/a[@class='hamburger']").click()
